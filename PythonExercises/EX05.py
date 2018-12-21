2c00226470153e695c585ef5ff1834512ad81ac7
Alessio Vaccaro <alexvac@hotmail.it>
2018-12-21 23:41:53 +0100

    Update and rename EX5.py to EX05.py

--- PythonExercises/EX05.py
+++ PythonExercises/EX05.py
@@ -1,25 +1,12 @@
 # Exercise 5
-#
-# Given 6
+
+# Write a code that given a number, let's say 6,  returns:
 # 1?2?3?4?5?6
 # 1?2?3?4?5
 # 1?2?3?4
 # 1?2?3
 # 1?2
 # 1
-
-##n=-1
-##while n<0:
-##    n=int(input('Digita numero positivo: '))
-##    if n<0:
-##        print('ridigita')
-##
-##for i in range(n,0,-1):
-##    stringa=str(1)
-##    for t in range(2,i+1):
-##        stringa+='?'+str(t)
-##    print(stringa)
-
 
 def getNumber():
     while True:
